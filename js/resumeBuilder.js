664dc2c6d9f128619350594598935d96bfccf938
Dan Frank <Dan Frank>
2016-05-06 11:38:10 -0400

    added var

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,7 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 
  $("#main").append(["Dan"]);
+
+ var awesomeThoughts = "I am Dan and i am AWESOME!";
+
+ console.log(awesomeThoughts);
