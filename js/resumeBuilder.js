1b9b57a20fadeed0021d96ab9c2bb2462fba7b47
Dan Frank <Dan Frank>
2016-05-11 11:04:43 -0400

    changed alot

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,11 +1,32 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var role = "Entry level programmer";
+var name = "Dan";
+var email = "danielfrank1015@gmail.com";
+var welcomeMsg = "Welcome!";
+var skills = ["Skills: ", "Java, ", "XML, ", "Python."];
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+var formattedContact = HTMLcontactGeneric.replace("%contact%", email);
+var formattedWelcome = HTMLwelcomeMsg.replace("%data%", welcomeMsg);
+var formattedSkillsStart = HTMLskillsStart.replace("%data%", "Skills: ");
 
 
 
- var awesomeThoughts = "I am Dan and i am AWESOME!";
 
- var funThoughts = awesomeThoughts.replace("AWESOME", "COOL");
+var bio = {
+	"name" : formattedName,
+	"role" : formattedRole,
+	"contact" : formattedContact,
+	"welcome" : formattedWelcome,
+	"skillsStart" : formattedSkillsStart
+};
 
- $("#main").append(funThoughts);
+
+$("#header").prepend(bio.welcome);
+$("#header").append(bio.name);
+$("#header").append(bio.role);
+$("#header").append(bio.skillsStart);
+$("#main").append(bio.contact);
